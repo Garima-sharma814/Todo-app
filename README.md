4c3da1f37d4e51d68e517b562317e9799d9cce95
Garima <sharmagarima814@gmail.com>
2021-06-02 18:18:55 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Todo-app
+# Todo-list-app
+Todo list app to track all the tasks we want to complete.
+## Prerequisite
+- Node 
+- NPM
+- Nodemon (optional)
+## Dependencies
+- Express
+- EJS
+- Body-Parser
+## Installation
+```
+npm i 
+```
---
+++ app.js
@@ -0,0 +1,47 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const date = require(__dirname+"/date.js");
+
+const app = express();
+
+var items=["Eat", "Code", "Sleep"];
+var workitems=[];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("static"));
+
+mongoose.connect("mongod://localhost:27017/todoDB");
+
+const port = 3000;
+const hostname = "127.0.0.1";
+
+// WeekEnd list
+app.get("/", (req, res) => {
+  let day = date();
+  res.render("list", { listName: day , addeditems: items} );
+});
+
+app.post("/", (req,res)=>{
+  var item = req.body.newItem;
+  if(req.body.list === "Work"){
+    workitems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+// Work list 
+app.get("/work", (req,res)=>{
+  res.render("list", {listName: "Work List", addeditems: workitems})
+});
+
+
+//Server 
+app.listen(port, () => {
+  console.log(`The server is running at http://${hostname}:${port}/`);
+});
